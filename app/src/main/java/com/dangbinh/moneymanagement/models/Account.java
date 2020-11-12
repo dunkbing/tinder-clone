6fa6c756c5562fa741cebd8f9ddaa5b662b7c9b7
binh <dangbinh4869@gmail.com>
2020-11-12 14:30:14 +0700

    added account model

---
+++ app/src/main/java/com/dangbinh/moneymanagement/models/Account.java
@@ -0,0 +1,42 @@
+package com.dangbinh.moneymanagement.models;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Account {
+    private String uid;
+    private String email;
+    private String password;
+    public final static FirebaseUser CURRENT_USER = FirebaseAuth.getInstance().getCurrentUser();
+
+    public Account() {}
+    public Account(String uid, String email, String password) {
+        this.uid = uid;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
