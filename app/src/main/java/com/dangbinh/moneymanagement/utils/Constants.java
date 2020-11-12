036e93f6544614e6b6d42501cf2ee1a252268075
binh <dangbinh4869@gmail.com>
2020-11-12 23:37:59 +0700

    refactor code

--- app/src/main/java/com/dangbinh/moneymanagement/utils/Constants.java
+++ app/src/main/java/com/dangbinh/moneymanagement/utils/Constants.java
@@ -2,6 +2,9 @@ package com.dangbinh.moneymanagement.utils;
 
 import android.graphics.drawable.Drawable;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
@@ -13,7 +16,9 @@ import java.util.regex.Pattern;
 public class Constants {
 
     public static String FIREBASE_URL = "https://prm391-demo.firebaseio.com/";
-
+    public final static FirebaseAuth AUTH_INSTANCE = FirebaseAuth.getInstance();
+    public final static FirebaseUser CURRENT_USER = AUTH_INSTANCE.getCurrentUser();
+    public final static String EXIT = "exit";
     public static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}$",
             Pattern.CASE_INSENSITIVE);
 
@@ -23,6 +28,6 @@ public class Constants {
     // ic_friends
     // ic_education
     // ic_debt
-    Map<String, Drawable> cat_images = new HashMap<String, Drawable>();
+    Map<String, Drawable> cateImages = new HashMap<>();
 
 }
