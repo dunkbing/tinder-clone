3e83d242c586d9697a60794db23729f04ab5f28e
NhatMinh <minh.nguyen@bluecoglobal.com>
2020-11-19 17:05:05 +0700

    .

--- app/src/main/java/com/dangbinh/moneymanagement/utils/WebUtils.java
+++
@@ -1,8 +0,0 @@
-package com.dangbinh.moneymanagement.utils;
-
-/**
- * Created by dangbinh on 9/11/2020.
- */
-public class WebUtils {
-    public static final String TAG = com.dangbinh.moneymanagement.utils.WebUtils.class.getName();
-}
